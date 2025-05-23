3d1608189df74190181a866b593e4c95d3cfd833
harunou <dev.harunou@gmail.com>
2025-05-23 09:15:21 +0200

    Types: add explicit types for UseCase result

--- src/@types/UseCase.ts
+++ src/@types/UseCase.ts
@@ -1,4 +1,7 @@
-export type UseCaseResult<T = void> = T | Error;
+export type UseCaseSuccess<T = void> = T;
+export type UseCaseFailure = Error;
+
+export type UseCaseResult<T = void> = UseCaseSuccess<T> | UseCaseFailure;
 
 export type UseCase<T = void, R = void> = {
   execute: (params: T) => Promise<UseCaseResult<R>>;
